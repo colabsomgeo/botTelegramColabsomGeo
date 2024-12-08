83ac31dc2a3f123346d75b481ccf4811f2c3bde6
Sannyer3232 <139788768+Sannyer3232@users.noreply.github.com>
2024-12-08 14:25:46 -0400

    Retirado codigo para descobrir ip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import requests
 import nest_asyncio
+nest_asyncio.apply()
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ConversationHandler
 from botTelegram.bot import handle_location, handle_audio, cancel, start, LOCATION, AUDIO
@@ -7,26 +7,7 @@ from botTelegram.bot import handle_location, handle_audio, cancel, start, LOCATI
 TOKEN = '7621244735:AAHYyFa1QI99bSLDl9jpwkB2L0QX10kQzwY'
 
 
-def get_public_ip():
-    try:
-        # Consulta o IP público usando uma API pública
-        response = requests.get("https://ifconfig.me")
-        response.raise_for_status()
-        return response.text.strip()
-    except requests.RequestException as e:
-        print(f"Erro ao obter o IP público: {e}")
-        return None
-
-
 def main():
-    # Obtém o IP público da instância Railway
-    public_ip = get_public_ip()
-    if public_ip:
-        print(f"IP público do Railway: {public_ip}")
-    else:
-        print("Não foi possível obter o IP público.")
-
-    nest_asyncio.apply()
     application = Application.builder().token(TOKEN).build()
 
     conv_handler = ConversationHandler(
@@ -38,9 +19,10 @@ def main():
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
+
     application.add_handler(conv_handler)
     application.run_polling()
-
-
+    
 if __name__ == "__main__":
     main()
+   
