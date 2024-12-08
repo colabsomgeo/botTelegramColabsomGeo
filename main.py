d780e7d8de6a6849d56a314af02e897823198cc9
Sannyer3232 <139788768+Sannyer3232@users.noreply.github.com>
2024-12-08 14:20:25 -0400

    Atualizado codigo para descobrir ip

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+import requests
 import nest_asyncio
-nest_asyncio.apply()
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ConversationHandler
 from botTelegram.bot import handle_location, handle_audio, cancel, start, LOCATION, AUDIO
-import requests
+
 TOKEN = '7621244735:AAHYyFa1QI99bSLDl9jpwkB2L0QX10kQzwY'
 
+
 def get_public_ip():
-    response = requests.get("https://api64.ipify.org?format=json")
-    return response.json()["ip"]
+    try:
+        # Consulta o IP público usando uma API pública
+        response = requests.get("https://ifconfig.me")
+        response.raise_for_status()
+        return response.text.strip()
+    except requests.RequestException as e:
+        print(f"Erro ao obter o IP público: {e}")
+        return None
+
 
 def main():
+    # Obtém o IP público da instância Railway
+    public_ip = get_public_ip()
+    if public_ip:
+        print(f"IP público do Railway: {public_ip}")
+    else:
+        print("Não foi possível obter o IP público.")
+
+    nest_asyncio.apply()
     application = Application.builder().token(TOKEN).build()
 
     conv_handler = ConversationHandler(
@@ -22,10 +38,9 @@ def main():
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
-    print("IP público:", get_public_ip())
     application.add_handler(conv_handler)
     application.run_polling()
-    
+
+
 if __name__ == "__main__":
     main()
-   
