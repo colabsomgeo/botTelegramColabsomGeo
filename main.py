70989004851bda81512e6b73395b27a8dae17b0a
Sannyer3232 <139788768+Sannyer3232@users.noreply.github.com>
2024-12-08 14:08:05 -0400

    Feito codigo para descobrir ip

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ nest_asyncio.apply()
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ConversationHandler
 from botTelegram.bot import handle_location, handle_audio, cancel, start, LOCATION, AUDIO
+import requests
 TOKEN = '7621244735:AAHYyFa1QI99bSLDl9jpwkB2L0QX10kQzwY'
+
+def get_public_ip():
+    response = requests.get("https://api64.ipify.org?format=json")
+    return response.json()["ip"]
 
 def main():
     application = Application.builder().token(TOKEN).build()
@@ -22,3 +27,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+    print("IP público:", get_public_ip())
